97257b6942510a82f6550a56e5ed68200a01021b
felizalde <coloelizalde@gmail.com>
2016-08-08 15:36:18 -0300

    create store and provider

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, useRouterHistory } from 'react-router'
+import { Provider } from 'react-redux'
 import { createHashHistory } from 'history'
+import { createStore } from 'redux'
 
 import Layout from './components/Layout'
 import Measures from './components/panes/Measures'
 import Sales from './components/panes/Sales'
 import About from './components/panes/About'
+
+import allReducers from './reducers/index'
+const store = createStore(allReducers);
 
 
 
@@ -16,12 +21,14 @@ const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 let main = document.getElementById('main');
 
 ReactDOM.render(
-  <Router history={appHistory}>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={Sales}></IndexRoute>
-      <Route path="measures" component={Measures}></Route>
-      <Route path="about" component={About}></Route>
-    </Route>
-  </Router>
+    <Provider store={store}>
+      <Router history={appHistory}>
+        <Route path="/" component={Layout}>
+          <IndexRoute component={Sales}></IndexRoute>
+          <Route path="measures" component={Measures}></Route>
+          <Route path="about" component={About}></Route>
+        </Route>
+      </Router>
+    </Provider>
 
   , main);
