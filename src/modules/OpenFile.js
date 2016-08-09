d5501fa27229b2d91931630b8c1b3a1bf92a0d91
felizalde <coloelizalde@gmail.com>
2016-08-09 19:42:44 -0300

    show dialog

--- src/modules/OpenFile.js
+++ src/modules/OpenFile.js
@@ -1,7 +1,11 @@
+'use strict';
 
-let openFile = () => {
-  console.log('Open File..');
+const {dialog} = window.require('electron').remote
+
+const openFile = () => {
+ dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']}, function(fileNames){
+     console.log(fileNames);
+ })
 }
-
 
 export default openFile;
