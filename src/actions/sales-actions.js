692c3fef42e285dab33c09105cef947d7a76d5b5
felizalde <coloelizalde@gmail.com>
2016-08-10 19:35:22 -0300

    async action to fetch sales file

--- src/actions/sales-actions.js
+++ src/actions/sales-actions.js
@@ -1,55 +1,15 @@
-export function fetchSales() {
-    return {
-        type:"FETCH_SALES_FULFILLED",
-        payload: [
-                {
-                id: '110',
-                items:['B','D']
-                },{
-                id: '120',
-                items:['A','B','D']
-                },{
-                id: '130',
-                items:['B','C','D']
-                },{
-                id: '140',
-                items:['D','F','H']
-                },{
-                id: '150',
-                items:['B','G','I']
-                },
-                {
-                id: '210',
-                items:['B','I']
-                },{
-                id: '220',
-                items:['A','B','I']
-                },{
-                id: '230',
-                items:['B','E','I']
-                },{
-                id: '240',
-                items:['B','C','E','I']
-                },{
-                id: '250',
-                items:['C','I']
-                },
-                {
-                id: '310',
-                items:['D','H']
-                },{
-                id: '320',
-                items:['D','F','H']
-                },{
-                id: '330',
-                items:['B','C','D','F']
-                },{
-                id: '340',
-                items:['H']
-                },{
-                id: '350',
-                items:['D','G','H']
-                }
-        ]
+import OpenFile from '../modules/open-file'
+
+export function fetchSales(){
+    return function(dispatch) {
+        dispatch({type:'FETCH_SALES'});
+        OpenFile()
+        .then((result)=>{
+            dispatch({type:'FETCH_SALES_FULFILLED', payload:result})
+        })
+        .catch((err) => {
+            dispatch({type:'FETCH_SALES_REJECTED', payload: err})
+        })
+
     }
 }
