d22e7cb7a8c1f6aab18be7bce73debc643798adf
felizalde <coloelizalde@gmail.com>
2016-11-08 10:41:45 -0300

    Showing measures only when there're results

--- src/components/panes/Measures.js
+++ src/components/panes/Measures.js
@@ -6,54 +6,61 @@ import Chart from './measures/Chart'
 import ListClusters from './measures/ListClusters'
 
 
-const data = [
-      {name: 'Cluster A', Small: 10, Medium: 13, Large: 40},
-      {name: 'Cluster B',  Small: 7, Medium: 3, Large: 16},
-      {name: 'Cluster C',  Small: 4, Medium: 6, Large: 13},
-      {name: 'Cluster D',  Small: 6, Medium: 4, Large: 22},
-      {name: 'Cluster E',  Small: 12, Medium: 11, Large: 32},
-      {name: 'Cluster F',  Small: 3, Medium: 16, Large: 36},
-      {name: 'Cluster G',  Small: 13, Medium: 13, Large: 27},
-];
-
 class Measures extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  generateDataChart() {
+    return this.props.algorithmData.clustering.map((cluster) => {
+      let small = cluster.smallI(this.props.algorithmData.ceiling).size;
+      let large = cluster.largeI(this.props.algorithmData.support).size;
+      return {
+        name   : 'Cluster ' + cluster.id,
+        Small  : small,
+        Medium : cluster.support.size - small - large,
+        Large  : large,
+      };
+    })
+  }
+
   render() {
+    if (!this.props.algorithmData.calculated) {
+       return (
+         <div>No se ha calculado..</div>
+       );
+    }
     return (
-        <div className="data-content">
-
-          <div className="market-details">
-            <FileDetails
-              filename = "Pepe"
-              sales = {160}
-              items = {130}
-              />
-            <AlgorithmDetails
-              clusters = {13}
-              ratio = {3.5}
-              ceiling = {0.3}
-              support = {0.7}
-              />
-            <Chart
-              data = {data}
-              />
+      <div className="data-content">
+        <div className="market-details">
+          <FileDetails
+            filename = "Pepe"
+            sales = {160}
+            items = {130}
+          />
+          <AlgorithmDetails
+            clusters = {this.props.algorithmData.clusters}
+            ratio = {this.props.algorithmData.threshold}
+            ceiling = {this.props.algorithmData.ceiling}
+            support = {this.props.algorithmData.support}
+          />
+          <Chart
+            data = {this.generateDataChart()}
+          />
           </div>
-
           <div className="list-clusters">
-            <ListClusters />
+            <ListClusters
+              clusters = {this.props.algorithmData.clustering}
+            />
           </div>
         </div>
-
-    );
+    )
   }
 }
 
 function mapStoreToProps(store) {
   return {
-    
+    algorithmData : store.slr
   };
 }
 
